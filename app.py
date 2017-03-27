129542a2736fcc17c5dcfae60a0bab11fffea4e4
Namita Dongre <ndongre898@gmail.com>
2017-03-26 22:02:09 -0400

    port stuff in app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, url_for, request, session, redirect
 import pymongo
 import json
 import datetime
+import os
 """import app"""
 
 app = Flask(__name__)
@@ -135,9 +136,10 @@ def global_():
     )
 
 if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
 	app.config['SESSION_TYPE'] = 'mongodb'
 	app.config['SECRET_KEY'] = 'secret!!!'
-	app.run(debug=True)
+	app.run(debug=True, port=port)
 
 """
 app = Flask(__name__)
